900542e1cf11580d63c4054e8f550c7c957dbcb4
Alexander <sanya.kashin.2000@mail.ru>
2022-10-30 03:43:12 +0300

    zad +

---
+++ zad1/Program.cs
@@ -0,0 +1,19 @@
+﻿/*
+Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+3, 5 -> 243 (3⁵)
+
+2, 4 -> 16
+*/
+System.Console.WriteLine("Введите число А: ");
+var A = int.Parse(Console.ReadLine()??"");
+
+System.Console.WriteLine("Введите число B: ");
+var B = int.Parse(Console.ReadLine()??"");
+
+var C = Math.Pow(A, B);
+System.Console.WriteLine($"число {A} в степени {B} будет равно - {C}");
+
+
+
+
---
+++ zad2/Program.cs
@@ -0,0 +1,23 @@
+﻿/*
+Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+452 -> 11
+
+82 -> 10
+
+9012 -> 12
+*/
+
+System.Console.WriteLine("Введите число");
+var num = int.Parse(Console.ReadLine()??"");
+var sum = 0;
+var c = num;
+
+while (c > 0)
+{
+    sum = sum + c % 10;
+    c = c / 10;
+} 
+System.Console.WriteLine(sum);
+
+
